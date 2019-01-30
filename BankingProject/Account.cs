1b77fc25e36d49485631211b17b9839b69093cb4
Kellan Arick <kellan5015@gmail.com>
2019-01-30 16:49:12 -0500

    Initial Commit

---
+++ BankingProject/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject
+{
+    class Account
+    {
+        private int Id { get; set; }
+        public int GetId()
+        {
+            return Id;
+        }
+        private string Description { get; set; }
+        public string GetDescription()
+        {
+            return Description;
+        }
+        public void SetDescription(string NewDescription)
+        {
+            Description = NewDescription;
+        }
+        private double Balance { get; set; }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+        public void Deposit(double Amount)
+        {
+            Balance += Amount;
+        }
+        public void Withdraw(double Amount)
+        {
+            Balance -= Amount;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Id = {Id}, Description = {Description}, Balance = {Balance}");
+        }
+        public Account(string NewDescription)
+        {
+            Id = -1;
+            Description = NewDescription;
+            Balance = 0;
+        }
+        public Account()
+        {
+            Id = -1;
+            Description = "New Account";
+            Balance = 0;
+        }
+    }
+}
--- BankingProject/Program.cs
+++ BankingProject/Program.cs
@@ -10,6 +10,12 @@ namespace BankingProject
     {
         static void Main(string[] args)
         {
-        }
+            var acct1 = new Account("Test Checking");
+            acct1.Print();
+            acct1.Deposit(1000.00);
+            acct1.Print();
+            acct1.Withdraw(60.00);
+            acct1.Print();
+        }  
     }
 }
